91379f1c5f4c7fefd60fffcd1ecd7b6e90cca184
paulr-gflcourses <paulgerald123@gmail.com>
2018-09-10 16:22:14 +0300

    dev rest

--- Server/api/cars/index.php
+++ Server/api/cars/index.php
@@ -1,3 +1,23 @@
 <?php
+include_once "../../libs/RestServer.php";
+
+function f()
+{
+
+    echo "--------- F --------------";
+
+}
+
+function getCars($server, $params)
+{
+    echo "GETTING CARS!";
+
+}
+
+$server = new RestServer();
 echo "hello server/api/cars";
+
+
+
+
 ?>
---
+++ Server/libs/RestServer.php
@@ -0,0 +1,50 @@
+<?php
+
+class RestServer
+{
+    public function __construct()
+    {
+        $url = $_SERVER['REQUEST_URI'];
+        list($a, $b, $c, $s, $a, $d, $db, $table, $path) = explode('/', $url, 9);
+        $params = explode('/', $url, 9);
+        # use setMethod to map to a specific function
+        # # GET /api/db/web/applist/test.com = getApplist(test.com)
+        echo "<pre>";
+        echo "parameters: s=$s, a=$a, d=$d, db=$db, table=$table, path=$path";
+        //call_user_func('f');
+        $method = $_SERVER['REQUEST_METHOD'];
+        echo "method: $method";
+        echo "params:";
+        print_r($params);
+        echo "</pre>";
+
+        switch($method)
+        {
+        case 'GET':
+        $this->setMethod('get'.ucfirst($table), explode('/', $path));
+        break;
+        case 'DELETE':
+        $this->setMethod('delete'.ucfirst($table), explode('/', $path));
+        break;
+        case 'POST':
+        $this->setMethod('post'.ucfirst($table), explode('/', $path));
+        break;
+        case 'PUT':
+        $this->setMethod('put'.ucfirst($table), explode('/', $path));
+        break;
+        default:
+        return false;
+        }
+
+    }
+    public function setMethod($method, $param = false)
+    {
+        echo "Method: $method, params:";
+            print_r($param);
+        if (method_exists($this, $method)) {
+            call_user_func($method, $param);
+            
+        }
+    }
+
+}
