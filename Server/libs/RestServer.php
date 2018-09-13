8c9928ea782c1f1b9d825b0e93f54958a0e446b9
paulr-gflcourses <paulgerald123@gmail.com>
2018-09-13 20:56:56 +0300

    dev rest

--- Server/libs/RestServer.php
+++ Server/libs/RestServer.php
@@ -8,8 +8,11 @@ class RestServer
     {
         $this->service = $service;
         $url = $_SERVER['REQUEST_URI'];
-        list($b, $c, $s, $a, $d, $db, $table, $path) = explode('/', $url, 8);
-        $params = explode('/', $url, 8);
+        //list($b, $c, $s, $a, $d, $db, $table, $path) = explode('/', $url, 8);
+        //$params = explode('/', $url, 8);
+
+        list( $c, $s, $a, $d, $db, $table, $path) = explode('/', $url, 7);
+        $params = explode('/', $url, 7);
         # use setMethod to map to a specific function
         # # GET /api/db/web/applist/test.com = getApplist(test.com)
         echo "<pre>";
@@ -18,37 +21,37 @@ class RestServer
         $method = $_SERVER['REQUEST_METHOD'];
         echo " method: $method,";
         //echo " params:";
-        //print_r($params);
-        
+        print_r($params);
+
 
         $funcName = ucfirst($table);
         $funcParams = explode('/', $path);
         switch ($method) 
         {
-            case 'GET':
-                //echo ", func params: ";
-                //print_r($funcParams);
-                $viewType = array_pop($funcParams);
-                $viewType = explode('?', $viewType)[0];
-                //echo ", viewType: $viewType";
-                $result = $this->setMethod('get' . $funcName, $funcParams);
-                $this->show_results($result, $viewType);
-                break;
-            case 'POST':
-                $this->setMethod('post' . $funcName, $funcParams);
-                break;
-            case 'PUT':
-                $this->setMethod('put' . $funcName, $funcParams);
-                break;
-            case 'DELETE':
-                $this->setMethod('delete' . $funcName, $funcParams);
-                break;
-            default:
-                return false;
+        case 'GET':
+            //echo ", func params: ";
+            //print_r($funcParams);
+            $viewType = array_pop($funcParams);
+            $viewType = explode('?', $viewType)[0];
+            //echo ", viewType: $viewType";
+            $result = $this->setMethod('get' . $funcName, $funcParams);
+            $this->show_results($result, $viewType);
+            break;
+        case 'POST':
+            $this->setMethod('post' . $funcName, $funcParams);
+            break;
+        case 'PUT':
+            $this->setMethod('put' . $funcName, $funcParams);
+            break;
+        case 'DELETE':
+            $this->setMethod('delete' . $funcName, $funcParams);
+            break;
+        default:
+            return false;
         }
         echo "</pre>";
     }
-    public function setMethod($funcName, $param = false)
+    private function setMethod($funcName, $param = false)
     {
         //echo "Method: $funcName, params:";
         //print_r($param);
@@ -62,18 +65,47 @@ class RestServer
         return $ret;
     }
 
-    public function show_results($result, $viewType)
+    private function show_results($result, $viewType)
     {
         //echo "viewType: $viewType";
         echo "\n Result: \n";
         switch ($viewType) 
         {
-            case '.json':
-                echo json_encode($result);
-                break;
-            default:
-                echo 'No such type!';
+        case '.json':
+            echo json_encode($result);
+            break;
+        case '.txt':
+            echo $this->objectToText($result);
+            break;
+        default:
+            echo 'No such type!';
         }
     }
+
+    private function objectToText($obj)
+    {
+        $res = "";
+        if (is_array($obj))
+        {
+            $first = $obj[0];
+            foreach ($first as $key=> $val) 
+            {
+                $res .= $key." \t"; 
+            }
+            foreach ($obj as $item) 
+            {
+                $res .= "\n";
+                foreach ($item as $field) 
+                {
+                    $res .= $field." \t";
+
+                }
+            }
+        }
+        return $res;
+
+    }
+
+
 
 }
